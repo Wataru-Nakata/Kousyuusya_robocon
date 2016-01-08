bf22b92ffbef6ea23bd9659ac972bf353098bb87
Wataru-Nakata <wataru9871@gmail.com>
2016-01-08 22:03:32 +0900

    Now servo will move within 180 degrees.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include "mbed.h"
 #include "Servo.h"
 #define servomax 90
-#define servomin 0
+#define servomin -90
 Serial pc (USBTX,USBRX);
 DigitalOut myled(LED1);
 Servo cam (D3);
 int x;
-float i = 0.5;
+float i = 0;
 int main() {
     while(1) {
         x = (pc.getc()/255.0)*640;
         if(x > 320){
-            i -=fabs(x-320)/64000.0*5*2;
+            i -=fabs(x-320)/(100.0);
         }else if(x< 320)
         {
-            i += fabs(320-x)/64000.0*5*2;
+            i += fabs(320-x)/(100.0);
         }
         if (i> servomax){
             i = servomax;
@@ -23,6 +23,6 @@ int main() {
             i = servomin;
         }
         cam.position(i);
-        pc.putc(char((i/90.0)*255));
+        pc.putc(char(((i+90)/180.0)*255));
     }
 }
